5b77ea2d92de576477aa9ad42c4cb3432c035a6c
jose perez <joserph.a@gmail.com>
2022-11-14 15:53:36 -0500

    Comenzando validaciones en js

--- public/assets/js/myjs.js
+++ public/assets/js/myjs.js
@@ -1,3 +1,16 @@
+const natural__person__form = document.getElementById('natural__person__form')
+const inputs = document.querySelectorAll('#natural__person__form input')
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+natural__person__form
+
 // ------------step-wizard-------------
 $(document).ready(function () {
     $('.nav-tabs > li a[title]').tooltip();
@@ -13,7 +26,8 @@ $(document).ready(function () {
     });
 
     $(".next-step").click(function (e) {
-        console.log('click');
+        /* Verificamos si estan llenos los campos requeridos */
+        
         var active = $('.wizard .nav-tabs li.active');
         active.next().removeClass('disabled');
         nextTab(active);
