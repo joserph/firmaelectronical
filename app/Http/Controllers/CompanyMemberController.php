2e43d3654462d3f47141d3f4c25ea87dd68f62cb
jose perez <joserph.a@gmail.com>
2022-12-30 14:29:41 -0500

    Agregamos Store

--- app/Http/Controllers/CompanyMemberController.php
+++ app/Http/Controllers/CompanyMemberController.php
@@ -37,7 +37,82 @@ class CompanyMemberController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'tipo_solicitud' => 'required',
+            'contenedor' => 'required|in:0,1,2',
+            'nombres' => 'required|string',
+            'apellido1' => 'required|string',
+            'apellido2' => 'string|nullable',
+            'tipodocumento' => 'required|in:CEDULA,PASAPORTE',
+            'numerodocumento' => 'required',
+            'coddactilar' => 'nullable|alpha_num',
+            //'ruc_personal' => 'nullable|numeric',
+            'sexo' => 'required|in:HOMBRE,MUJER',
+            'fecha_nacimiento' => 'required|date',
+            'nacionalidad' => 'required|string',
+            'telfCelular' => 'required|numeric',
+            'telfCelular2' => 'required|numeric',
+            'telfFijo' => 'nullable',
+            'eMail' => 'required|email',
+            'eMail2' => 'required|email',
+            'empresa' => 'required',
+            'ruc_empresa' => 'required|numeric',
+            'cargo' => 'required|string',
+            'motivo' => 'required|string',
+            'unidad' => 'required|string',
+            'provincia' => 'required|string',
+            'ciudad' => 'required|string',
+            'direccion' => 'required',
+            'nombresRL' => 'required|string',
+            'apellidosRL' => 'required|string',
+            'tipodocumentoRL' => 'required|in:CEDULA,PASAPORTE',
+            'numerodocumentoRL' => 'required',
+            'vigenciafirma' => 'required|in:7 días,1 año,2 años,3 años,4 años,5 años',
+            'express' => 'required|in:Si,No',
+            'f_cedulaFront' => 'required|image|mimes:png,jpg',
+            'f_cedulaBack' => 'required|image|mimes:png,jpg',
+            'f_selfie' => 'required|image|mimes:png,jpg',
+            'f_copiaruc' => 'required|mimes:pdf',
+            'f_nombramiento' => 'required|mimes:pdf',
+            'f_nombramiento2' => 'nullable|mimes:pdf',
+            'f_constitucion' => 'required|mimes:pdf',
+            'f_documentoRL' => 'required|mimes:pdf',
+            'f_autreprelegal' => 'required|mimes:pdf',
+            'f_adicional1' => 'max:4000',
+            'f_adicional2' => 'nullable',
+            'f_adicional3' => 'nullable',
+            'f_adicional4' => 'nullable',
+            'mismos_datos_factu' => 'required|in:Si,No',
+            'fecha_deposito' => 'nullable|date',
+            'num_deposito' => 'nullable|alpha_num',
+            'nombre_banco' => 'nullable|string',
+            'nombre_depositante' => 'nullable|string',
+            'estatus_pago' => 'nullable',
+            'nombre_partner' => 'nullable|alpha_num',
+            'ruc_cedula_factura' => 'nullable|numeric',
+            'nombres_factura' => 'nullable|string',
+            'correo_factura' => 'nullable|email',
+            'direccion_factura' => 'nullable|alpha_num',
+            'telefono_factura' => 'nullable|numeric',
+            'comentarios_factura' => 'nullable|alpha_num',
+            'fecha_ingreso' => 'nullable|date',
+            'fecha_envio' => 'nullable|date',
+            'estatus' => 'nullable',
+        ]);
+
+        $company_member = CompanyMember::create($request->all());
+
+        if(Auth::check()){
+            if($company_member)
+            {
+                return redirect()->route('company-member.index')->with('save', 'true');
+            } 
+        }else{
+            if($company_member)
+            {
+                return redirect()->route('company-member.create')->with('save', 'true');
+            } 
+        }
     }
 
     /**
@@ -48,7 +123,9 @@ class CompanyMemberController extends Controller
      */
     public function show($id)
     {
-        //
+        $company_member = CompanyMember::find($id);
+
+        return view('company-member.show', compact('company_member'));
     }
 
     /**
